0c74336e2ce6b69950bfadddc55a591885108ebc
EncryptedV100 <marwanlecourtabram91671@gmail.com>
2025-09-18 21:47:21 +0200

    Update Gamelist.lua

--- Gamelist.lua
+++ Gamelist.lua
@@ -1,7 +1,7 @@
 local Games = {
   [3623096087] = "https://raw.githubusercontent.com/EncryptedV100/Paradox-Hub/refs/heads/main/Roblox%20Scripts/Muscle%20Legends.lua", -- Muscle Legends
-  [3101667897] = "", -- Legends of Speed City
-  [3232996272] = "", -- Legends Of Speed Space/Speed Desert
-  [3276265788] = "", -- Legends Of Speed Space/Speed Desert
+  [3101667897] = "https://raw.githubusercontent.com/EncryptedV100/Paradox-Hub/refs/heads/main/Roblox%20Scripts/Legends%20Of%20Speed.lua", -- Legends of Speed City
+  [3232996272] = "https://raw.githubusercontent.com/EncryptedV100/Paradox-Hub/refs/heads/main/Roblox%20Scripts/Legends%20Of%20Speed.lua", -- Legends Of Speed Space/Speed Desert
+  [3276265788] = "https://raw.githubusercontent.com/EncryptedV100/Paradox-Hub/refs/heads/main/Roblox%20Scripts/Legends%20Of%20Speed.lua", -- Legends Of Speed Space/Speed Desert
 }
 return Games
