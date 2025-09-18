748a13ed3b0a3b9038682107ffbce93344a173f3
EncryptedV100 <marwanlecourtabram91671@gmail.com>
2025-09-18 21:45:32 +0200

    Create Gamelist.lua

---
+++ Gamelist.lua
@@ -0,0 +1,7 @@
+local Games = {
+  [3623096087] = "", -- Muscle Legends
+  [3101667897] = "", -- Legends of Speed City
+  [3232996272] = "", -- Legends Of Speed Space/Speed Desert
+  [3276265788] = "", -- Legends Of Speed Space/Speed Desert
+}
+return Games
