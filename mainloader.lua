acd5b267ee8055c1034fcd8a95203bf64a7460a0
EncryptedV100 <marwanlecourtabram91671@gmail.com>
2025-09-23 20:10:56 +0200

    Update mainloader.lua

--- mainloader.lua
+++ mainloader.lua
@@ -1,1 +1,7 @@
+local Games = loadstring(game:HttpGet("https://raw.githubusercontent.com/EncryptedV100/Paradox-Hub/refs/heads/main/Gamelist.lua"))()
 
+for PlaceID, Execute in pairs(Games) do
+    if PlaceID == game.PlaceId then
+        loadstring(game:HttpGet(Execute))()
+    end
+end
