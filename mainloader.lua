2b83e658acf3419296ed94406ed1e08c382cdd87
EncryptedV100 <marwanlecourtabram91671@gmail.com>
2025-10-05 22:20:01 +0200

    Update mainloader.lua

--- mainloader.lua
+++ mainloader.lua
@@ -1,4 +1,4 @@
-loadstring(game:HttpGet('https://raw.githubusercontent.com/EncryptedV100/Paradox-Hub/refs/heads/main/Universal%20Scripts/Anti_Afk.lua'))()
+loadstring(game:HttpGet('https://paradox-protector.vercel.app/api/paradox-protected/n4o5p6q7r8s9t0u1v2w3x4y5z6'))()
 local Games = loadstring(game:HttpGet("https://raw.githubusercontent.com/EncryptedV100/Paradox-Hub/refs/heads/main/Gamelist.lua"))()
 
 for PlaceID, Execute in pairs(Games) do
