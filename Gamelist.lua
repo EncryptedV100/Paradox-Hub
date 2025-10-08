27b5f2d799f4b29f2406eff78ee73c1882d9e3b0
EncryptedV100 <marwanlecourtabram91671@gmail.com>
2025-10-08 14:55:28 +0200

    Update Gamelist.lua

--- Gamelist.lua
+++ Gamelist.lua
@@ -3,5 +3,6 @@ local Games = {
   [3101667897] = "https://raw.githubusercontent.com/EncryptedV100/Paradox-Hub/refs/heads/main/Roblox%20Scripts/Legends%20Of%20Speed.lua", -- Legends of Speed City
   [3232996272] = "https://raw.githubusercontent.com/EncryptedV100/Paradox-Hub/refs/heads/main/Roblox%20Scripts/Legends%20Of%20Speed.lua", -- Legends Of Speed Space/Speed Desert
   [3276265788] = "https://raw.githubusercontent.com/EncryptedV100/Paradox-Hub/refs/heads/main/Roblox%20Scripts/Legends%20Of%20Speed.lua", -- Legends Of Speed Space/Speed Desert
+  [109983668079237] ) "https://raw.githubusercontent.com/EncryptedV100/Paradox-Hub/refs/heads/main/Roblox%20Scripts/Steal%20A%20Brainrot.lua" Steal A Brainrot
 }
 return Games
