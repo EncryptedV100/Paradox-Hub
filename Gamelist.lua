870dbfbb7cdd20229ec064c3368e8855a760a5ba
EncryptedV100 <marwanlecourtabram91671@gmail.com>
2025-10-08 22:00:16 +0200

    Update Gamelist.lua

--- Gamelist.lua
+++ Gamelist.lua
@@ -1,8 +1,9 @@
 local Games = {
-  [3623096087] = "https://raw.githubusercontent.com/EncryptedV100/Paradox-Hub/refs/heads/main/Roblox%20Scripts/Muscle%20Legends.lua", -- Muscle Legends
-  [3101667897] = "https://raw.githubusercontent.com/EncryptedV100/Paradox-Hub/refs/heads/main/Roblox%20Scripts/Legends%20Of%20Speed.lua", -- Legends of Speed City
-  [3232996272] = "https://raw.githubusercontent.com/EncryptedV100/Paradox-Hub/refs/heads/main/Roblox%20Scripts/Legends%20Of%20Speed.lua", -- Legends Of Speed Space/Speed Desert
-  [3276265788] = "https://raw.githubusercontent.com/EncryptedV100/Paradox-Hub/refs/heads/main/Roblox%20Scripts/Legends%20Of%20Speed.lua", -- Legends Of Speed Space/Speed Desert
-  [109983668079237] ) "https://raw.githubusercontent.com/EncryptedV100/Paradox-Hub/refs/heads/main/Roblox%20Scripts/Steal%20A%20Brainrot.lua" Steal A Brainrot
+    [3623096087] = "https://raw.githubusercontent.com/EncryptedV100/Paradox-Hub/refs/heads/main/Roblox%20Scripts/Muscle%20Legends.lua", -- Muscle Legends
+    [3101667897] = "https://raw.githubusercontent.com/EncryptedV100/Paradox-Hub/refs/heads/main/Roblox%20Scripts/Legends%20Of%20Speed.lua", -- Legends of Speed City
+    [3232996272] = "https://raw.githubusercontent.com/EncryptedV100/Paradox-Hub/refs/heads/main/Roblox%20Scripts/Legends%20Of%20Speed.lua", -- Legends Of Speed Space/Speed Desert
+    [3276265788] = "https://raw.githubusercontent.com/EncryptedV100/Paradox-Hub/refs/heads/main/Roblox%20Scripts/Legends%20Of%20Speed.lua", -- Legends Of Speed Space/Speed Desert
+    [109983668079237] = "https://raw.githubusercontent.com/EncryptedV100/Paradox-Hub/refs/heads/main/Roblox%20Scripts/Steal%20A%20Brainrot.lua", -- Steal A Brainrot
 }
+
 return Games
